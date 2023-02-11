3d8248504b141a063463c2c65563bbace878d8f3
uh-zz <uhzz.contact@gmail.com>
2023-02-11 20:45:54 +0900

    refactor: sqload, add interface

---
+++ sqload.go
@@ -0,0 +1,68 @@
+package sqload
+
+import (
+	"bytes"
+	"embed"
+	"io/fs"
+
+	_ "github.com/pingcap/tidb/parser/test_driver"
+)
+
+type Loader struct {
+	d Dialector
+}
+
+func New(dialector Dialector) Loader {
+	return Loader{d: dialector}
+}
+
+func (l Loader) Load(content *embed.FS) (string, error) {
+	fileNames, err := getAllFilenames(content)
+	if err != nil {
+		return "", err
+	}
+
+	var buf bytes.Buffer
+	for _, name := range fileNames {
+		file, err := content.ReadFile(name)
+		if err != nil {
+			return "", err
+		}
+		buf.Write(file)
+	}
+
+	return buf.String(), nil
+}
+
+func (l Loader) Parse(sqlfile string) ([]string, error) {
+	var (
+		sqls []string
+		err  error
+	)
+
+	if dialector, ok := l.d.(interface {
+		Parse(string) ([]string, error)
+	}); ok {
+		if sqls, err = dialector.Parse(sqlfile); err != nil {
+			return nil, err
+		}
+	}
+	return sqls, nil
+}
+
+func getAllFilenames(efs *embed.FS) ([]string, error) {
+	var files []string
+	if err := fs.WalkDir(efs, ".", func(path string, d fs.DirEntry, err error) error {
+		if d.IsDir() {
+			return nil
+		}
+
+		files = append(files, path)
+
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	return files, nil
+}
