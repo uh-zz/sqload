9456d18fa8c8ae92d33e4f009e1b0be1ce9d8730
uh-zz <uhzz.contact@gmail.com>
2023-02-12 22:38:49 +0900

    update: test parallel

--- sqload_test.go
+++ sqload_test.go
@@ -114,7 +114,11 @@ func TestLoader_Parse(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
+
+			t.Parallel()
+
 			l := Loader{
 				d: tt.fields.d,
 			}
